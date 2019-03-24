87b0a49c87914956011418b4622823d8517b3005
kirangolla <kiran.golla@quantiphi.com>
2019-03-24 20:16:29 +0530

    Updates

---
+++ precache-manifest.2189f15bae1ef57b2762d3962cf2795e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "af0341c921623083eb6d",
+    "url": "/react-app/static/css/main.780f810b.chunk.css"
+  },
+  {
+    "revision": "af0341c921623083eb6d",
+    "url": "/react-app/static/js/main.c9032a5f.chunk.js"
+  },
+  {
+    "revision": "9ed022644c6f38b380ac",
+    "url": "/react-app/static/js/runtime~main.cbc86c42.js"
+  },
+  {
+    "revision": "43db52a970a9a8c56042",
+    "url": "/react-app/static/js/2.82a15eca.chunk.js"
+  },
+  {
+    "revision": "e4944c1985443801707d6e04d04a654d",
+    "url": "/react-app/index.html"
+  }
+];
